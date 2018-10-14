8afd13f4c16724b713aecb662109c47c18bba8a2
Akarumbra <32853966+Akarumbra@users.noreply.github.com>
2018-10-14 18:34:05 +0200

    Intento de cargar animaciones + mapa 2
    Un intento de cargar las animaciones de la rana y el mapa 2

--- Motor2D/j1Player.cpp
+++ Motor2D/j1Player.cpp
@@ -11,6 +11,61 @@
 j1Player::j1Player() : j1Module()
 {
 	name.create("player");
+	bool j1Player::awake(const pugi::xml_node & frog) {
+
+		frog.front_idle_loop = frog.child("frog").child("animation").child("front_idle").attribute("loop").as_bool();
+		frog.front_idle_speed = frog.child("frog").child("front_idle").attribute("speed").as_float();
+
+		pugi::xml_node it = frog.child("frog").child("animation").child("front_idle").first_child;
+		while (it != nullptr)
+		{
+			SDL_Rect rect;
+
+			rect.x = it.attribute("x").as_int();
+			rect.y = it.attribute("y").as_int();
+			rect.w = it.attribute("w").as_int();
+			rect.h = it.attribute("h").as_int();
+
+			front_idle.push_back(rect);
+
+			it.next_sibling();
+	}
+
+		frog.side_idle_loop = frog.child("frog").child("animation").child("side_idle").attribute("loop").as_bool();
+		frog.side_idle_speed = frog.child("frog").child("side_idle").attribute("speed").as_float();
+
+		pugi::xml_node it = frog.child("frog").child("animation").child("side_idle").first_child;
+		while (it != nullptr)
+		{
+			SDL_Rect rect;
+
+			rect.x = it.attribute("x").as_int();
+			rect.y = it.attribute("y").as_int();
+			rect.w = it.attribute("w").as_int();
+			rect.h = it.attribute("h").as_int();
+
+			side_idle.push_back(rect);
+
+			it.next_sibling();
+		}
+
+		frog.jump = frog.child("frog").child("animation").child("jump").attribute("loop").as_bool();
+		frog.side_idle_speed = frog.child("frog").child("jump").attribute("speed").as_float();
+
+		pugi::xml_node it = frog.child("frog").child("animation").child("jump").first_child;
+		while (it != nullptr)
+		{
+			SDL_Rect rect;
+
+			rect.x = it.attribute("x").as_int();
+			rect.y = it.attribute("y").as_int();
+			rect.w = it.attribute("w").as_int();
+			rect.h = it.attribute("h").as_int();
+
+			jump.push_back(rect);
+
+			it.next_sibling();
+		}
 }
 
 // Destructor
--- Motor2D/j1Player.h
+++ Motor2D/j1Player.h
@@ -1,10 +1,11 @@
-#ifndef __j1PLAYER_H__
+﻿#ifndef __j1PLAYER_H__
 #define __j1PLAYER_H__
 
 #include "PugiXml/src/pugixml.hpp"
 #include "p2List.h"
 #include "p2Point.h"
 #include "j1Module.h"
+#include "Animation.h"
 
 class j1Player : public j1Module
 {
@@ -13,6 +14,11 @@ public:
 
 	// Destructor
 	virtual ~j1Player();
+	bool awake::(const pugi::xml_node & frog); //no sé cómo añadir el archivo frog para que lo detecte
+
+private:
+
+	pugi::xml_document frog;
 };
 
 #endif // __j1PLAYER_H__
